4923430399b47982534349a10377670f07445685
pavel-paulau <pavel.paulau@gmail.com>
2013-06-12 15:18:06 +0300

    fetch benchmarl metadata from database

--- main.go
+++ main.go
@@ -1,20 +1,45 @@
 package main
 
 import (
+	"github.com/couchbaselabs/go-couchbase"
 	"github.com/hoisie/mustache"
 	"github.com/hoisie/web"
 )
 
-func home() string {
-	var num_benchmarks = 0
-	var benchmarks = ""
+var c, _ = couchbase.Connect("http://127.0.0.1:8091/")
+var pool, _ = c.GetPool("default")
+var b_metrics, _ = pool.GetBucket("metrics")
+var b_clusters, _ = pool.GetBucket("clusters")
 
-	for i := 0; i < num_benchmarks; i++ {
-		var benchmark = map[string]string{}
-		benchmarks += mustache.RenderFile("templates/benchmark.mustache", benchmark)
+func get_benchmarks() []map[string]interface{} {
+	res, _ := b_metrics.View("metrics", "all", map[string]interface{}{
+		"stale": false,
+	})
+
+	benchmarks := []map[string]interface{}{}
+	for _, row := range res.Rows {
+		benchmark := row.Value.(map[string]interface{})
+		benchmark["id"] = row.ID
+
+		rv := map[string]string{}
+		b_clusters.Get(benchmark["cluster"].(string), &rv)
+		for k, v := range rv {
+			benchmark[k] = v
+		}
+		benchmarks = append(benchmarks, benchmark)
+	}
+	return benchmarks
+}
+
+func home() string {
+	var benchmarks = get_benchmarks()
+	var content = ""
+
+	for _, benchmark := range benchmarks {
+		content += mustache.RenderFile("templates/benchmark.mustache", benchmark)
 	}
 
-	return mustache.RenderFile("templates/home.mustache", map[string]string{"benchmarks": benchmarks})
+	return mustache.RenderFile("templates/home.mustache", map[string]string{"content": content})
 }
 
 func main() {
