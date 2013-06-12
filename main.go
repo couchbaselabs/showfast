377ec555d0db298d66de6a390bf5584102d7c076
pavel-paulau <pavel.paulau@gmail.com>
2013-06-12 13:22:51 +0300

    create basic UI for list of benchmarks

--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"github.com/hoisie/mustache"
 	"github.com/hoisie/web"
 )
 
-func index() string {
-	return "Performance dashboard"
+func home() string {
+	var num_benchmarks = 0
+	var benchmarks = ""
+
+	for i := 0; i < num_benchmarks; i++ {
+		var benchmark = map[string]string{}
+		benchmarks += mustache.RenderFile("templates/benchmark.mustache", benchmark)
+	}
+
+	return mustache.RenderFile("templates/home.mustache", map[string]string{"benchmarks": benchmarks})
 }
 
 func main() {
-	web.Get("/", index)
+	web.Get("/", home)
 	web.Run("127.0.0.1:8080")
 }
