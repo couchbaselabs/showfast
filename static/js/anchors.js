d80ff5b6c69f1be0d51e879922eaa7ac1c42220c
pavel-paulau <pavel.paulau@gmail.com>
2013-07-12 13:42:40 +0300

    revert "add persistent anchors for filters"
    This reverts commit 84690da663d5646b88bb3d33e2b149ca9485c568.

--- static/js/anchors.js
+++
@@ -1,7 +0,0 @@
-var hash = window.location.hash.substring(1);
-if (hash) {
-	$("#" + hash).button('toggle');
-	$("div[id*='cont_" + hash + "']").css("visibility", "visible");
-} else {
-	$(".metric").css("visibility", "visible");
-}
