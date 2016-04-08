d8d51d0d3dae4fe92b031db967cd1a92d0f58d2e
sandipnd <sandip.nandi@couchbase.com>
2016-04-08 12:30:38 -0700

    showfast change to make Fts -> FTS

--- app/js/controller.js
+++ app/js/controller.js
@@ -76,7 +76,7 @@ function MetricList($scope, $http) {
 		}, {
 			"id": "subdoc", "title": "Sub Doc"
 		}, {
-			"id": "fts", "title": "Fts"
+			"id": "fts", "title": "FTS"
 		}
 		];
 
@@ -229,7 +229,7 @@ function MetricList($scope, $http) {
 		},{
 			"id": "index", "title": "Index"
 		},{
-                        "id": "elastic", "title": "Elastic"
+                        "id": "elastic", "title": "ElasticSearch"
                 }
 		];
 
